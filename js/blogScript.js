d755b7acdd5aaa54da44363c5f9af7cbd101b229
LavY <148194874+YuliaKushnir@users.noreply.github.com>
2024-02-06 19:03:16 +0200

    added pages

---
+++ js/blogScript.js
@@ -0,0 +1,60 @@
+function readMoreBtn1(){
+    let more1 = document.getElementById("more1");
+    let btnReadMore1 = document.getElementById("readMoreBtn1");
+
+    if(more1.style.display === "inline"){
+        btnReadMore1.innerHTML = "читати далі...";
+        more1.style.display="none";
+    } else {
+        btnReadMore1.innerHTML = "приховати";
+        more1.style.display="inline";
+    }
+}
+function readMoreBtn2(){
+    let more = document.getElementById("more2");
+    let btnReadMore = document.getElementById("readMoreBtn2");
+
+    if(more.style.display === "inline"){
+        btnReadMore.innerHTML = "читати далі...";
+        more.style.display="none";
+    } else {
+        btnReadMore.innerHTML = "приховати";
+        more.style.display="inline";
+    }
+}
+function readMoreBtn3(){
+    let more = document.getElementById("more3");
+    let btnReadMore = document.getElementById("readMoreBtn3");
+
+    if(more.style.display === "inline"){
+        btnReadMore.innerHTML = "читати далі...";
+        more.style.display="none";
+    } else {
+        btnReadMore.innerHTML = "приховати";
+        more.style.display="inline";
+    }
+}
+function readMoreBtn4(){
+    let more = document.getElementById("more4");
+    let btnReadMore = document.getElementById("readMoreBtn4");
+
+    if(more.style.display === "inline"){
+        btnReadMore.innerHTML = "читати далі...";
+        more.style.display="none";
+    } else {
+        btnReadMore.innerHTML = "приховати";
+        more.style.display="inline";
+    }
+}
+function readMoreBtn5(){
+    let more = document.getElementById("more5");
+    let btnReadMore = document.getElementById("readMoreBtn5");
+
+    if(more.style.display === "inline"){
+        btnReadMore.innerHTML = "читати далі...";
+        more.style.display="none";
+    } else {
+        btnReadMore.innerHTML = "приховати";
+        more.style.display="inline";
+    }
+}
